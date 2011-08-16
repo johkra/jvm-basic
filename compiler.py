d6390de395ec6194307d20b56dea9aedc8521bc3
Johannes Krampf <johannes.krampf@gmail.com>
2011-08-16 18:39:48 +0200

    Add support for string variables

--- compiler.py
+++ compiler.py
@@ -12,6 +12,12 @@ from class_file import ClassFile
 from BASIC_parser import parse_to_AST
 from lib.pyPEG import Symbol
 
+ALOAD = b"\x19"
+ASTORE = b"\x3a"
+GETSTATIC = b"\xb2"
+LDC = b"\x12"
+INVOKEVIRTUAL = b"\xb6"
+
 def create_code(file_name, message):
     """Write the compile code to a class file"""
 
@@ -22,6 +28,7 @@ class Compiler:
         self.functions = functions
         self.code = ClassFile(self.class_name, self.file_name)
         self.method_bytecode = b""
+        self.vars = {"args": 0};
 
     def parse(self, _fileinput):
         self.AST = parse_to_AST(_fileinput)
@@ -37,24 +44,42 @@ class Compiler:
                     function(self, tree.what)
             self._tree_walker(tree.what)
 
+    def _add_var(self, var_name):
+        if var_name in self.vars:
+            return self.vars[var_name]
+        idx = len(self.vars)
+        self.vars[var_name] = idx
+        return idx
+
     def save(self):
         method_bytecode = b"\x2a\xb7" + struct.pack("!h",
             self.code.add_method_to_const_pool("java/lang/Object", "<init>", "()V")
         ) + b"\xb1"
         self.code.add_method(0x0000, "<init>", "()V", 1, 1, method_bytecode)
         self.method_bytecode += b"\xb1"
-        self.code.add_method(0x0009, "main", "([Ljava/lang/String;)V", 2, 1, self.method_bytecode)
+        self.code.add_method(0x0009, "main", "([Ljava/lang/String;)V", 2, len(self.vars), self.method_bytecode)
 
         open(self.class_name + ".class", "wb").write(self.code.write_class())
 
-def print_statement(self,args):
-    string_value = args[0].what[0]
-    if string_value.__name__ == "string":
-        field_print_stream = self.code.add_field_to_const_pool("java/lang/System", "out", "Ljava/io/PrintStream;")
-        print_value = self.code.add_string_ref_to_const_pool(string_value.what[0])
-        method_print_stream = self.code.add_method_to_const_pool("java/io/PrintStream", "println", "(Ljava/lang/String;)V")
-        self.method_bytecode += b"\xb2" + struct.pack("!h", field_print_stream) + b"\x12" + struct.pack("B", print_value) + b"\xb6" + struct.pack("!h", method_print_stream)
+def load_string_value(self, node):
+    if node.__name__ == "string":
+        print_value = self.code.add_string_ref_to_const_pool(node.what[0])
+        return LDC + struct.pack("B", print_value)
+    return ALOAD + struct.pack("B", self.vars[node.what])
 
+def print_statement(self, args):
+    field_print_stream = self.code.add_field_to_const_pool("java/lang/System", "out", "Ljava/io/PrintStream;")
+
+    method_print_stream = self.code.add_method_to_const_pool("java/io/PrintStream", "println", "(Ljava/lang/String;)V")
+    self.method_bytecode += GETSTATIC + struct.pack("!h", field_print_stream)
+    self.method_bytecode += load_string_value(self, args[0].what[0])
+    self.method_bytecode += INVOKEVIRTUAL + struct.pack("!h", method_print_stream)
+
+def string_assignment(self, args):
+    var_name = args[0].what
+    var = self._add_var(var_name)
+    self.method_bytecode += load_string_value(self, args[1].what[0])
+    self.method_bytecode += ASTORE + struct.pack("B", self.vars[var_name])
 
 def main():
     """Main function. Compiles a basic file to a class file."""
@@ -64,7 +89,7 @@ def main():
     messages = []
 
     file_name = sys.argv[1]
-    compiler = Compiler(file_name, [print_statement])
+    compiler = Compiler(file_name, [print_statement, string_assignment])
     compiler.parse(fileinput.input())
     compiler.save()
 
