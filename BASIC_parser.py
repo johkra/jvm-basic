63c4d0f39a7ff0bd250d56e5fa37f4fc0991a8d6
Johannes Krampf <johannes.krampf@gmail.com>
2011-08-17 09:11:11 +0200

    Remove commented out example code from grammar

--- BASIC_parser.py
+++ BASIC_parser.py
@@ -6,22 +6,6 @@ import re
 
 from lib.pyPEG import parse, keyword, _and, _not
 
-#def comment():          return [re.compile(r"//.*"), re.compile("/\*.*?\*/", re.S)]
-#def literal():          return re.compile(r'\d*\.\d*|\d+|".*?"')
-#def symbol():           return re.compile(r"\w+")
-#def operator():         return re.compile(r"\+|\-|\*|\/|\=\=")
-#def operation():        return symbol, operator, [literal, functioncall]
-#def expression():       return [literal, operation, functioncall]
-#def expressionlist():   return expression, -1, (",", expression)
-#def returnstatement():  return keyword("return"), expression
-#def ifstatement():      return keyword("if"), "(", expression, ")", block, keyword("else"), block
-#def statement():        return [ifstatement, returnstatement], ";"
-#def block():            return "{", -2, statement, "}"
-#def parameterlist():    return "(", symbol, -1, (",", symbol), ")"
-#def functioncall():     return symbol, "(", expressionlist, ")"
-#def function():         return keyword("function"), symbol, parameterlist, block
-#def simpleLanguage():   return function
-
 def _comment():
     return [re.compile(r"'.*"), re.compile(r"REM.*"), re.S]
 
